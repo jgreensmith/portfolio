48b7fa9db3fdccf1307e4d98178a38553f3b4c78
James Greensmith <jgreensmith13@gmail.com>
2022-02-02 23:19:16 +0000

    main button

--- components/Hero.js
+++ components/Hero.js
@@ -6,6 +6,9 @@ import 'swiper/css/autoplay';
 import 'swiper/css/effect-fade';
 import { HeroBox, HeroSlide } from '../utils/styles';
 import { urlFor } from '../sanity';
+//import UnstyledButtonCustom from "./svg/ButtonSvg";
+import SvgButton from './svg/ButtonSvg';
+
 
 const params = {
     slidesPerView: 'auto',
@@ -35,7 +38,8 @@ const Hero = ({ heroData }) => {
                             backgroundImage: `url("${urlFor(hero.heroImage).quality(90).fit("min").url()}")`
                         }} 
                     >
-
+                        <SvgButton>Test button</SvgButton>
+                        {/* <UnstyledButtonCustom /> */}
                     </HeroSlide>
                 </SwiperSlide>
             ))}
